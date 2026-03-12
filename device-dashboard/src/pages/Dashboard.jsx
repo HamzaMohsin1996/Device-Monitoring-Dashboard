f6d5c4c302c232cd1cdf1aac99d9f29bcf13919c
HamzaMohsin1996 <hamza95.hw@gmail.com>
2026-03-12 01:24:26 +0100

    adding search filtering

--- device-dashboard/src/pages/Dashboard.jsx
+++ device-dashboard/src/pages/Dashboard.jsx
@@ -3,27 +3,60 @@ import { fetchDevices } from "../services/api";
 import DeviceCard from "../components/DeviceCard";
 
 function Dashboard() {
+
   const [devices, setDevices] = useState([]);
-  const [loading, setLoading] = useState(true);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [statusFilter, setStatusFilter] = useState("All");
 
   useEffect(() => {
     fetchDevices().then((data) => {
       setDevices(data);
-      setLoading(false);
     });
   }, []);
 
-  if (loading) return <p>Loading devices...</p>;
+  const filteredDevices = devices.filter((device) => {
+
+    const matchesSearch =
+      device.device.toLowerCase().includes(searchTerm.toLowerCase());
+
+    const matchesStatus =
+      statusFilter === "All" || device.status === statusFilter;
+
+    return matchesSearch && matchesStatus;
+
+  });
 
   return (
     <div>
+
       <h1>Device Monitoring Dashboard</h1>
 
+      <div className="controls">
+
+        <input
+          type="text"
+          placeholder="Search device..."
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+
+        <select
+          value={statusFilter}
+          onChange={(e) => setStatusFilter(e.target.value)}
+        >
+          <option value="All">All</option>
+          <option value="Online">Online</option>
+          <option value="Offline">Offline</option>
+        </select>
+
+      </div>
+
       <div className="device-grid">
-        {devices.map((device) => (
+        {filteredDevices.map((device) => (
           <DeviceCard key={device.id} device={device} />
         ))}
       </div>
+
     </div>
   );
 }
